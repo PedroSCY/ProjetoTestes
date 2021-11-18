fe8efda1b015dee0009f1fa54b63f3fbfecdd773
PedroSCY <hock.gonzagas7@gmail.com>
2021-11-18 16:56:16 -0300

    Projeto Base

---
+++ ProjetoTestes/src/main/java/com/tt/ProjetoTestes/ProjetoTestesApplication.java
@@ -0,0 +1,13 @@
+package com.tt.ProjetoTestes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTestesApplication.class, args);
+	}
+
+}
